f766b245ead942347fcd500f72b8fe150293f588
jj <jjtara@gmail.com>
2017-01-12 16:31:07 +0100

    Paso 3 Creando animaciones para diferentes tipos de eventos

--- www/js/gestures.js
+++ www/js/gestures.js
@@ -24,7 +24,32 @@ var app={
     hammertime.get('pinch').set({ enable: true});
     hammertime.get('rotate').set({ enable: true});
 
-    hammertime.on('tap doubletap pan swipe press pinch rotate', function(ev) { document.querySelector('#info').innerHTML= ev.type+ '!'; });
+    zona.addEventListener('webkit-AnimationEnd', function(e){
+      zona.className='';
+    });
+
+    hammertime.on('doubletap', function(ev) {
+      zona.className='doubletap';
+    });
+
+    hammertime.on('press', function(ev) {
+      zona.className='press';
+    });
+
+    hammertime.on('swipe', function(ev) {
+      var clase=undefined;
+      direccion=ev.direction;
+
+      if (direccion==4) clase='swipe-derecha';
+      if (direccion==2) clase='swipe-izquierda';
+      zona.className=clase;
+    });
+
+    hammertime.on('rotate', function(ev) {
+      var umbral=25;
+      if (ev.distance > umbral) zona.className='rotate';
+    });
+
   },
 
  ponloClaro: function(){
